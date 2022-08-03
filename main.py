cef76bc2d2d61ac72b66e053d196439751996070
Omar Hantouli <101668359+omarShantouli@users.noreply.github.com>
2022-08-03 18:17:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import smtplib
 import datetime
-my_email = "samigogo339@gmail.com"
-password = "samigogo1990"
+import os
+
+my_email = os.environ.get('EMAIL')
+password = os.environ.get('PASSWORD')
+address = os.environ.get('ADDRESS_EMAIL')
 
 if datetime.datetime.now() == datetime.datetime(2026, 10, 8):
     with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
@@ -9,7 +12,7 @@ if datetime.datetime.now() == datetime.datetime(2026, 10, 8):
         connection.login(user=my_email, password=password)
         connection.sendmail(
             from_addr=my_email,
-            to_addrs="hantoli797@gmail.com",
+            to_addrs=address,
             msg="Subject:License Reminder"
                 "\n\n Your License will be expire after a week from now"
         )
@@ -21,7 +24,7 @@ elif datetime.datetime.now() >= datetime.datetime(2026, 10, 15):
         connection.login(user=my_email, password=password)
         connection.sendmail(
             from_addr=my_email,
-            to_addrs="hantoli797@gmail.com",
+            to_addrs=address,
             msg="Subject:License Reminder"
                 "\n\n Your License is expired "
         )
